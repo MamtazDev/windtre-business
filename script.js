8616ff459cc48863331c1e13544d020be07cf7ab
mohi14 <mohiuddinkhan1409@gmail.com>
2023-05-25 13:54:45 +0600

    some changes done

--- script.js
+++ script.js
@@ -11,17 +11,29 @@ var countdown = setInterval(function () {
 
   // Calculate the days, hours, minutes, and seconds
   var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  var dayss = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+
   var hours = Math.floor(
     (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
+  var hourss = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  var minutess = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  var secondss = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
   // Display the countdown
   document.getElementById("days").textContent = days;
   document.getElementById("hours").textContent = hours;
   document.getElementById("minutes").textContent = minutes;
   document.getElementById("seconds").textContent = seconds;
+
+  document.getElementById("dayss").textContent = dayss;
+  document.getElementById("hourss").textContent = hourss;
+  document.getElementById("minutess").textContent = minutess;
+  document.getElementById("secondss").textContent = secondss;
 
   // If the countdown is finished, display a message
   if (timeRemaining < 0) {
@@ -30,10 +42,15 @@ var countdown = setInterval(function () {
     document.getElementById("hours").textContent = "0";
     document.getElementById("minutes").textContent = "0";
     document.getElementById("seconds").textContent = "0";
+    
+    document.getElementById("dayss").textContent = "0";
+    document.getElementById("hourss").textContent = "0";
+    document.getElementById("minutess").textContent = "0";
+    document.getElementById("secondss").textContent = "0";
   }
 }, 1000);
 
 function decreaseLabelScale(labelId) {
-    var label = document.getElementById(labelId);
-    label.classList.add("label-small");
-  }
+  var label = document.getElementById(labelId);
+  label.classList.add("label-small");
+}
