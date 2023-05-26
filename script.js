9e0394bc5c8c19370129ba80f1c36eff24339b0e
mohi14 <mohiuddinkhan1409@gmail.com>
2023-05-26 15:14:34 +0600

    updated

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ var countdown = setInterval(function () {
     document.getElementById("hours").textContent = "0";
     document.getElementById("minutes").textContent = "0";
     document.getElementById("seconds").textContent = "0";
-    
+
     document.getElementById("dayss").textContent = "0";
     document.getElementById("hourss").textContent = "0";
     document.getElementById("minutess").textContent = "0";
@@ -54,3 +54,98 @@ function decreaseLabelScale(labelId) {
   var label = document.getElementById(labelId);
   label.classList.add("label-small");
 }
+
+$(document).ready(function () {
+  $("#myForm").submit(function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    // Validate the form
+    if (validateForm()) {
+      // If the form is valid, submit it
+      this.submit();
+      $("#exampleModal2").modal("show");
+    } else {
+      // Show the modal if validation fails
+      $("#exampleModal").modal("show");
+    }
+  });
+
+  function validateForm() {
+    let isValid = true;
+
+    // Clear any previous error messages
+    $(".error-message").remove();
+
+    // Validate Name and Surname field
+    const nameInput = $("#nameInput");
+    const nameLabel = $("#nameLabel");
+    if (nameInput.val().trim() === "") {
+      isValid = false;
+      nameLabel.html("Name is required");
+    }
+
+    // Validate Telephone field
+    const telephoneInput = $("#telephoneInput");
+    const telephoneLabel = $("#telephoneLabel");
+    const telephonePattern = /^\d{10}$/; // Regular expression for a 10-digit phone number
+    if (telephoneInput.val().trim() === "") {
+      isValid = false;
+      telephoneLabel.html("Telephone is required");
+    } else if (!telephonePattern.test(telephoneInput.val().trim())) {
+      isValid = false;
+      telephoneLabel.html("Invalid phone number");
+    }
+
+    // Validate VAT number field
+    const vatInput = $("#vatInput");
+    const vatLabel = $("#vatLabel");
+    const vatPattern = /^[A-Za-z]{2}\d{11}$/; // Regular expression for a VAT number with 2 letters followed by 11 digits
+    if (vatInput.val().trim() !== "") {
+      if (!vatPattern.test(vatInput.val().trim())) {
+        isValid = false;
+        vatLabel.html("Invalid VAT number");
+      } else {
+        // Make API request to validate VAT number
+        const apiUrl = "https://www.vatcomply.com/vat-validation-api/"; // Replace with your API endpoint
+        const requestData = {
+          vatNumber: vatInput.val().trim(),
+        };
+
+        $.ajax({
+          url: apiUrl,
+          method: "POST",
+          data: requestData,
+          dataType: "json",
+          async: false, // Make the request synchronous for simplicity
+          success: function (response) {
+            if (!response.isValid) {
+              isValid = false;
+              vatLabel.html("Invalid VAT number");
+            }
+          },
+          error: function (xhr, status, error) {
+            console.log(error);
+          },
+        });
+      }
+    }
+
+    // Validate Required field
+    const requiredInput = $("#requiredInput");
+    if (requiredInput.val().trim() === "") {
+      isValid = false;
+      requiredInput.html("This field is required");
+    }
+
+    // Validate the checkbox
+    const checkbox = $("#flexCheckDefault");
+    if (!checkbox.prop("checked")) {
+      isValid = false;
+      // checkbox.after(
+      //   '<span class="error-message">You must accept the processing of personal data</span>'
+      // );
+    }
+
+    return isValid;
+  }
+});
